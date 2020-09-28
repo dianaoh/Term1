3e20ad26bb32022cdb861cd2291c133822561a9f
dianaoh <dianaoh@10.1.103.166>
2020-09-28 10:37:59 -0400

    commit

---
+++ Term1/src/LineSlope.java
@@ -0,0 +1,44 @@
+public class LineSlope {
+	// you'll probably want some instance variables... (4 coordinates and a slope)
+	double xOne, xTwo, yOne, yTwo, slope;
+
+
+		// use the parameters to assign your instance variables
+		public void set_coordinates (double x1, double y1, double x2, double y2) {
+			xOne = x1;
+			yOne = y1;
+			xTwo = x2;
+			yTwo = y2;
+					
+		}
+
+		
+		// calculate the slope using your instance variables
+		public void calculate_slope () {
+			slope = (yOne - yTwo)/(xOne - xTwo);
+		}
+
+		
+		// print the slope you calculated in calculate_slope
+		public void display_slope () {
+			
+			System.out.println(slope);
+		}
+		
+		public static void main(String[] args) {
+			
+			LineSlope runner = new LineSlope();
+			
+			// this line should have a slope of 2
+			runner.set_coordinates(3, 3, 7, 11);
+			runner.calculate_slope();
+			runner.display_slope();
+			
+			// this line should have a slope of -.75
+			runner.set_coordinates(0, -2, -4, 1);
+			runner.calculate_slope();
+			runner.display_slope();
+
+		}
+	}
+
