83738d0065ed9f3b3e7f06600a536ed4cade446e
dianaoh <dianaoh@10.1.103.166>
2020-10-02 11:43:33 -0400

    COmitt

---
+++ Term1/src/HumanMethods.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class HumanMethods {
+	private int age;
+	private char gender;
+	
+	public void setAge (int num)
+	{
+		age = num;
+	}
+	
+	public void setGender (char l)
+	{
+		gender = l;
+	}
+	
+	public void vote () {
+		if (age>18)
+		{
+			System.out.println("person can vote");
+		}
+	}
+	
+	public void tetanus ()
+	{
+		if (age%4==0)
+		{
+			System.out.println("need tetanus shot");
+		}
+	}
+	
+	public void toddler () {
+		if (age<4 && gender== 'g') {
+			System.out.println("toddler girl");
+		}
+		else if (age< 4 && gender=='b') {
+			System.out.println("toddler boy");
+		}
+		else {
+			System.out.println("not toddler");
+		}
+	}
+		
+	public void discount () {
+		if (age>65 || age<12)
+		{
+			System.out.println("discount");
+		}
+	}
+		
+	public void teen () {
+		if (age>12 && age<18)
+		{
+			System.out.println("teenager");
+		}
+	}
+	
+	public void teammates (int a1, int g1) { //Challenge
+		if((a1-age<-2 || a1-age<2) && g1==gender) {
+			System.out.println("teammates");
+		}
+	}
+
+	
+	public static void main(String [] args) {
+		HumanMethods a = new HumanMethods ();
+		a.setAge (3);
+		a.setGender('g');
+		a.tetanus ();
+		a.vote();
+		a.toddler ();
+		a.discount ();
+		a.teen();
+		a.teammates(1, 'g');
+	}
+
+}
