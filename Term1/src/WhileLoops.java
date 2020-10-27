0653e34f7037c48224a45deaf5d60af32196346d
dianaoh <dianaoh@localhost>
2020-10-27 11:43:44 -0400

    commit

---
+++ Term1/src/WhileLoops.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class WhileLoops {
+	Scanner scanner= new Scanner (System.in);
+	
+	//definite finite
+	public void include() {
+		System.out.println("enter two numbers, first smaller than the second");
+		int first=scanner.nextInt ();
+		int second=scanner.nextInt();
+		while (first<= second) {
+			System.out.println(first);
+			first++;	
+		}
+	}
+	//definite finite
+	public void exponential() {
+		System.out.println("enter two numbers");
+		int x=0;
+		double first=scanner.nextDouble ();
+		double second=scanner.nextDouble();
+		while (x<6) {
+			System.out.println(second);
+			second = first*second;
+			x++;
+		}
+	}
+	
+	//definite finite
+	public void root() {
+		System.out.println("enter a number");
+		int num=scanner.nextInt ();
+		int x=0;
+		while (x*x<=num) {
+			if (x*x==num) {
+				System.out.println(x);
+				break;
+			}
+			x++;
+		}
+		
+		if (x*x!=num) {
+			System.out.println("does not have root");
+		}
+	}
+	public static void main(String args[]) {
+		
+		WhileLoops a= new WhileLoops ();
+		a.include ();
+		a.exponential();
+		a.root();
+		
+	}
+
+}
