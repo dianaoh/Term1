f896bdcc4a9b4007face4eeb636c03b2e0bc544b
dianaoh <dianaoh@10.1.103.166>
2020-11-04 02:10:47 -0500

    comit

--- Term1/src/Projects/Pong.java
+++ Term1/src/Projects/Pong.java
@@ -24,17 +24,15 @@ public class Pong extends JPanel implements KeyListener {
 	// constants that are predefined and won't change as the program runs
 	private final int WIDTH = 600, HEIGHT = 600, WINDOW_HEIGHT = 650;
 	private final int PADDLE_WIDTH = 20, DIAM = 8, PADDLE_HEIGHT = 100;
-	private final int SLOWER_DIAM=20;
-	private final int SLOWER_SPEED=5;
+	private final int SLOWER_DIAM=15;
 	
-	private int paddle_speed1 = 3,paddle_speed2=3;
-	private int x= WIDTH/2, y= HEIGHT/2, speedX= 2, speedY = 3;
+	private  int slower_speed=5;
+	private int paddle_speed1=3, paddle_speed2=3;
+	private int x= WIDTH/2+3, y= HEIGHT/2-10, speedX= 0, speedY = 0;
 	private int paddle1 = WIDTH/2-PADDLE_WIDTH/2, paddle2= WIDTH/2-PADDLE_WIDTH/2;
 	private int score1, score2;
-	private int slower_count1=1, slower_count2=1;
 	private int slower_position1=0, slower_position2=WIDTH-SLOWER_DIAM;
-	private int slower_positionx2=paddle2+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
-	private int slower_positionx1=paddle1+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
+	private int slower_positionx1= paddle1+PADDLE_HEIGHT/2-SLOWER_DIAM/2, slower_positionx2= paddle2+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
 
 
 	
@@ -43,9 +41,8 @@ public class Pong extends JPanel implements KeyListener {
 	private boolean solo1 = false, solo2=false;
 	private boolean round =false;
 	private boolean restart=false;
-	private boolean slower1=false, slower2=false, display1=true;
-	private boolean display;
-	
+	private boolean slower1=false, slower2=false;
+	private boolean starter=true;
 	
 	
 	// this method moves the ball at each timestep
@@ -65,6 +62,7 @@ public class Pong extends JPanel implements KeyListener {
 		if (up2==true)
 		{
 			paddle2 += paddle_speed2;
+			
 		}
 		
 		if (down1 == true) {
@@ -81,10 +79,10 @@ public class Pong extends JPanel implements KeyListener {
 	//this method moves the slowballs at each timestep
 	public void move_slowballs () {
 		if (slower1==true) {
-			slower_position1+=SLOWER_SPEED;
+			slower_position1+=slower_speed;
 		}
 		if (slower2==true) {
-			slower_position2-=SLOWER_SPEED;
+			slower_position2-=slower_speed;
 		}
 	}
 	
@@ -95,12 +93,14 @@ public class Pong extends JPanel implements KeyListener {
 		
 		if (x>=WIDTH-DIAM) {
 			score1++;
+			slower_speed=2;
 			newRound ();
 			
 		}
 		
 		else if (x<=0) {
 			score2++;
+			slower_speed=2;
 			newRound ();
 		}
 		
@@ -123,10 +123,10 @@ public class Pong extends JPanel implements KeyListener {
 			up1=true;
 			down1=false;
 		}
+		
 		if (paddle1+PADDLE_HEIGHT>=HEIGHT && solo1==true) {
 			down1=true;
 			up1=false;
-			
 		}
 		
 		if (paddle2<0 && solo2==true) {
@@ -138,15 +138,16 @@ public class Pong extends JPanel implements KeyListener {
 			down2=true;
 			up2=false;
 		}
+		
 		if (slower_position2<=PADDLE_WIDTH && slower_positionx2>=paddle1 && slower_positionx2<= paddle1+PADDLE_HEIGHT) {
-			paddle_speed1 = 1;
-			slower1=false;
+			paddle_speed1=1;
 		}
 		
 		if (slower_position1>=WIDTH-PADDLE_WIDTH-DIAM && slower_positionx1>= paddle2 && slower_positionx1<= paddle2+PADDLE_HEIGHT) {
-			paddle_speed2 = 1;
-			slower2=false;
+			paddle_speed2=1;
 		}
+		
+		
 	}
 	
 
@@ -155,11 +156,23 @@ public class Pong extends JPanel implements KeyListener {
 	// you simply need to fill in a few parameters here
 	public void paint(Graphics g) {
 		// background color is gray
-		g.setColor(Color.LIGHT_GRAY);
+		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
+		if (starter==true) {
+			Font a= new Font ("Arial",Font.BOLD,100);
+			g.setFont(a);
+			g.setColor(Color.white);
+			g.drawString("PONG", WIDTH/4,HEIGHT/2);
+			Font b= new Font ("Arial",Font.ITALIC,20);
+			g.setFont(b);
+			g.setColor(Color.white);
+			g.drawString("Press 'q'(1) or the left arrow key(2) for slow balls", WIDTH/7,HEIGHT/2+40);
+			g.drawString("Press 'd' to continue", WIDTH/3,HEIGHT/2+20);
+		}
+		
 		// draw your rectangles and circles here
-		g.setColor(new Color(50,100,200));
+		g.setColor(new Color(255,255,255));
 		g.fillOval(x,y,DIAM,DIAM);
 		
 		g.setColor(new Color (255,255,255));
@@ -169,22 +182,26 @@ public class Pong extends JPanel implements KeyListener {
 		g.fillRect (WIDTH-PADDLE_WIDTH, paddle2 ,PADDLE_WIDTH, PADDLE_HEIGHT);
 		
 		//Balls to make the other paddle slower
-		g.setColor(new Color (12,12,24));
+		g.setColor(new Color (255,255,255));
+		int slower_positionx1= paddle1+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
 		g.fillOval(slower_position1,slower_positionx1,SLOWER_DIAM,SLOWER_DIAM);
 		
-		g.setColor(new Color (12,12,24));
+		g.setColor(new Color (255,255,255));
+		int slower_positionx2= paddle2+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
 		g.fillOval(slower_position2,slower_positionx2,SLOWER_DIAM,SLOWER_DIAM);
 		
 		// writes the score of the game - you just need to fill the scores in
+		
 		Font f = new Font("Arial", Font.BOLD, 14);
 		g.setFont(f);
-		g.setColor(Color.black);
+		g.setColor(Color.white);
 		g.drawString("P1 Score: "+score1, WIDTH/5, 20);
 		g.drawString("P2 Score: "+score2, WIDTH*3/5, 20);
 		FontMetrics fontMetrics = g.getFontMetrics(f);
 		
 		//New Round
 		if (round==true) {
+			slower_speed=0;
 			String sentance= "Press c to continue";
 			int size = fontMetrics.stringWidth(sentance);
 			g.drawString(sentance, WIDTH/2-size/2, HEIGHT/2);
@@ -210,14 +227,8 @@ public class Pong extends JPanel implements KeyListener {
 		if (solo2==true) {
 			String sentance= "Solo2 Mode";
 			int size= fontMetrics.stringWidth(sentance);
-			g.drawString(sentance, WIDTH-size/8,20);
-			
-		if (display==false) {
-			String sentance1= "No more balls left";
-			int size1= fontMetrics.stringWidth(sentance1);
-			g.drawString(sentance1, WIDTH-size1/8,40);
-		}
-			
+			g.drawString(sentance, WIDTH-(size+size/8),20);
+
 		}
 	}
 
@@ -244,29 +255,26 @@ public class Pong extends JPanel implements KeyListener {
 			restart=true;
 		}
 		
+		if (e.getKeyChar()=='d') {
+			starter=false;
+			speedX=3;
+			speedY=4;
+		}
+		
+		//slowball shooting
 		if (e.getKeyChar()=='q') {
-			slower_count1--;
-			if (slower_count1<0) {
-				display=false;
-			}
-			else {
-				slower1=true;
-			}
+			slower_speed=5;
+			slower1=true;
 		}
 		if (keyCode==KeyEvent.VK_LEFT){
-			slower_count2--;
+			slower_speed=5;
 			slower2=true;
-			if (slower_count2<0) {
-				display=false;
-			}
-			else {
-				slower2=true;
-			}
 		}
 		
 		// turn 1-player mode on
 		if (e.getKeyChar() == '1') {
 			solo1 = true;
+			solo2=false;
 			solo();
 		}
 
@@ -274,6 +282,7 @@ public class Pong extends JPanel implements KeyListener {
 		// turn 2-player mode on
 		if (e.getKeyChar() == '2') {
 			solo2= true;
+			solo1=false;
 			solo ();
 			
 		}
@@ -283,25 +292,25 @@ public class Pong extends JPanel implements KeyListener {
 				down1=true;
 			}
 			else if (solo2==true) {
-				down2=true;
+				up2=true;
 			}
 			
-			speedX=2;
-			speedY=3;
+			speedX=3;
+			speedY=4;
 			round=false;
 		}
 		
 		if (e.getKeyChar()=='n') {
 			restart=false;
-			speedX=2;
-			speedY=3;
+			speedX=3;
+			speedY=4;
 		}
 		
 		if (e.getKeyChar()=='y') {
 			restart ();
 			restart=false;
-			speedX=2;
-			speedY=3;
+			speedX=3;
+			speedY=4;
 		}
 		
 		
@@ -332,8 +341,10 @@ public class Pong extends JPanel implements KeyListener {
 	
 	// restarts the game, including scores
 	public void restart() {
-	    slower_positionx1= paddle1+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
-		slower_positionx2= paddle2+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
+		slower_position1=0;
+		slower_position2=WIDTH-SLOWER_DIAM;
+		paddle_speed1=3;
+		paddle_speed2=3;
 		score1=0;
 		score2=0;
 		speedX=0;
@@ -345,13 +356,16 @@ public class Pong extends JPanel implements KeyListener {
 	}
 	
 	public void newRound () {
-		slower_positionx1= paddle1+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
-		slower_positionx2= paddle2+PADDLE_HEIGHT/2-SLOWER_DIAM/2;
+		slower_position1=0;
+		slower_position2=WIDTH-SLOWER_DIAM;
+		paddle_speed1=3;
+		paddle_speed2=3;
 		round=true;
 		up1=false;
 		up2=false;
 		down1=false;
 		down2=false;
+		slower_speed=0;
 		speedX=0;
 		speedY=0;
 		x=WIDTH/2;
@@ -362,18 +376,21 @@ public class Pong extends JPanel implements KeyListener {
 	}
 	
 	public void solo () {
+		slower_speed=0;
 		round=false;
 		restart=false;
 		score1=0;
 		score2=0;
-		speedX=2;
-		speedY=3;
+		speedX=3;
+		speedY=4;
 		if (solo1==true) {
-			down1=true;
+			solo2=false;
+			up1=true;
 			
 		}
 		else if (solo2==true) {
-			down2=true;
+			solo1=false;
+			up2=true;
 			
 		}
 	}
