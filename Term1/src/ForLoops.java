d3023fbe817b28c898d196a2dbf42a2820e2fd76
dianaoh <dianaoh@10.1.103.166>
2020-11-10 18:26:23 -0500

    commit

--- Term1/src/ForLoops.java
+++ Term1/src/ForLoops.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ForLoops {
 	Scanner scanner= new Scanner (System.in);
 	
+	///Print out the sum of all numbers from 1 to x (x is a parameter).
 	public void Easy2 (int x) {
 		int sum=0;
 		for (int i=0;i<=x;i++) {
@@ -11,6 +12,7 @@ public class ForLoops {
 		System.out.println(sum);
 	}
 	
+	//Ask the user for answers until they give the right solution to the riddle.
 	public void Easy3 () {
 		System.out.println("What digit is the most frequent between the numbers 1 and 1,000 (inclusive)?");
 		int answer= scanner.nextInt();
@@ -21,6 +23,7 @@ public class ForLoops {
 		System.out.println("Congradulations you are correct!");
 	}
 	
+	//Print out the multiplication table for 1-12.
 	public void Hard1() {
 		for (int i=1;i<=12;i++) {
 			for (int j=1;j<=12;j++) {
@@ -30,6 +33,7 @@ public class ForLoops {
 		}
 	}
 	
+	//Print out all prime numbers from 1 to x 
 	public void Hard2(int x) {
 		int count=0;
 		for (int i=1;i<=x;i++) {
@@ -46,7 +50,7 @@ public class ForLoops {
 		
 	}
 	
-	
+	//Print out an “X” of stars that is n rows tall 
 	public void Hard4 (int n) {
 		for (int i=1;i<=n;i++) {
 			for (int j=0;j<=n;j++) {
